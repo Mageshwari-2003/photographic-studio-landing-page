1e810a178dcd660f517116d4251d4d3ab6af96e9
Your Name <you@example.com>
2025-06-11 01:14:17 -0700

    d

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+const menuBtn = document.getElementById("menu-btn"); 
+const navLinks = document.getElementById('nav_links');
+const menuBtnIcon = document.querySelector('.nav_menu_btn i'); 
+
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains('open');
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", () => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+
+ const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+//container
+ScrollReveal().reveal(".container_content h1", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".container_content h2", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".container_content p", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".social", { 
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
